e5a2db5ded2365649cfa57d1c5eab0a5bc6784e5
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-26 23:21:14 +0200

    Correct URL and add 2.47 driver back

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -6,7 +6,10 @@ set -x
 wget -N http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
 shasum --check selenium-server-standalone-2.47.1.jar.sha1
 
-wget -N http://selenium-release.storage.googleapis.com/2.47/IEDriverServer_Win32_2.46.0.zip
+wget -N http://selenium-release.storage.googleapis.com/2.47/IEDriverServer_Win32_2.47.0.zip
+shasum --check IEDriverServer_Win32_2.47.0.zip.sha1
+
+wget -N http://selenium-release.storage.googleapis.com/2.46/IEDriverServer_Win32_2.46.0.zip
 shasum --check IEDriverServer_Win32_2.46.0.zip.sha1
 
 wget -N --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-windows-i586.exe
@@ -14,4 +17,3 @@ shasum --check jre-8u60-windows-i586.exe.sha1
 
 wget -N --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-windows-x64.exe
 shasum --check jre-8u60-windows-x64.exe.sha1
-
