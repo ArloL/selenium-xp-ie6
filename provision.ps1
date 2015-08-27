05de6c1db600d3c60a9a2956b645ce79785f1e97
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-27 16:58:13 +0200

    Add some more options to java install

--- provision.ps1
+++ provision.ps1
@@ -10,7 +10,7 @@ if ([IntPtr]::size -eq 4) {
 netsh firewall set opmode disable
 
 if (!(Test-Path "C:\Program Files\Java\jre1.8.0_60")) {
-    Start-Process -FilePath "$($scriptDir)\jre-8u60-windows-$($java_architecture).exe" -ArgumentList "/s" -Wait -Passthru
+    Start-Process -FilePath "$($scriptDir)\jre-8u60-windows-$($java_architecture).exe" -ArgumentList "/s","INSTALL_SILENT=1","AUTO_UPDATE=0","WEB_JAVA=0","REBOOT=0","NOSTARTMENU=1","SPONSORS=0" -Wait -Passthru
 }
 
 Unzip "$($scriptDir)\IEDriverServer_Win32_2.46.0.zip" "C:\Windows\system32" $true
