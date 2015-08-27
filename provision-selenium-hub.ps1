083c6242e71b4c55ea217b4973aee290a35955a7
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-27 23:24:11 +0200

    Use scriptDir

--- provision-selenium-hub.ps1
+++ provision-selenium-hub.ps1
@@ -1,3 +1,5 @@
+$scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+
 if (Test-Path "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup") {
-    Copy-Item "C:\vagrant\selenium-hub.bat" "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup" -Force
+    Copy-Item "$($scriptDir)\selenium-hub.bat" "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup" -Force
 }
