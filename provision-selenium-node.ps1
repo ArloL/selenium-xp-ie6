0ab7ae0bd760e895cd11655a41d4b723eacc2aad
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-27 23:24:39 +0200

    Provision selenium node with Ip parameter

--- provision-selenium-node.ps1
+++ provision-selenium-node.ps1
@@ -1,3 +1,12 @@
+param([String]$ip)
+
+$bat = @'
+@echo off
+start java -jar C:\selenium\selenium-server-standalone-2.47.1.jar -role node -hub http://{ip}:4444/grid/register
+'@
+
+$bat = $bat.Replace("{ip}", $ip)
+
 if (Test-Path "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup") {
-    Copy-Item "C:\vagrant\selenium-node.bat" "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup" -Force
+    $bat | Out-File "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup\selenium-node.bat" -Force
 }
