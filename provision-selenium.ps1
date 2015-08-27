6550eca6509614d7f5dbfc201a91267f5ed8f209
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-27 22:47:02 +0200

    Use ALLUSERSPROFILE environment variable

--- provision-selenium.ps1
+++ provision-selenium.ps1
@@ -2,4 +2,6 @@ netsh firewall set opmode disable
 
 md C:\selenium -Force
 Copy-Item "C:\vagrant\selenium-server-standalone-2.47.1.jar" "C:\selenium"
-Copy-Item "C:\vagrant\selenium.bat" "C:\Documents and Settings\All Users\Start Menu\Programs\Startup" -Force
+if (Test-Path "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup") {
+    Copy-Item "C:\vagrant\selenium.bat" "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup" -Force
+}
