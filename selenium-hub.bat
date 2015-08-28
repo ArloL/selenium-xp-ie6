9872a2aa9230b894010beb40dab43e9ed0c25d42
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-28 09:52:41 +0200

    Remove selenium-hub.bat

--- selenium-hub.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start java -jar C:\selenium\selenium-server-standalone-2.47.1.jar -role hub
-ping -n 15 127.0.0.1 >nul
-start java -jar C:\selenium\selenium-server-standalone-2.47.1.jar -role node -hub http://localhost:4444/grid/register
