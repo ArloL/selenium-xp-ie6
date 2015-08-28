af7cca497d5aa7a6dc843fe33de1aa8b4346c611
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2015-08-28 09:53:09 +0200

    Update provision-selenium-node.ps1

--- provision-selenium-node.ps1
+++ provision-selenium-node.ps1
@@ -1,11 +1,12 @@
-param([String]$ip)
+param([String]$hubIp, [String]$bindIp)
 
 $bat = @'
 @echo off
-start java -jar C:\selenium\selenium-server-standalone-2.47.1.jar -role node -hub http://{ip}:4444/grid/register
+java -jar C:\selenium\selenium-server-standalone-2.47.1.jar -role node -hub http://{hubIp}:4444/grid/register -host {bindIp}
 '@
 
-$bat = $bat.Replace("{ip}", $ip)
+$bat = $bat.Replace("{hubIp}", $hubIp)
+$bat = $bat.Replace("{bindIp}", $bindIp)
 
 if (Test-Path "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup") {
     $bat | Out-File "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup\selenium-node.bat" -Encoding "Default" -Force
