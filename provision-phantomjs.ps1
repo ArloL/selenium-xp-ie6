2d024be70c1905f3bff097bdd00b499b605c1389
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-28 17:02:19 +0200

    Add firewall exception for 1.9.8

--- provision-phantomjs.ps1
+++ provision-phantomjs.ps1
@@ -5,3 +5,5 @@ $scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
 md C:\opt -Force
 
 Unzip "$($scriptDir)\phantomjs-1.9.8-windows.zip" "C:\opt" $true
+
+netsh firewall add allowedprogram program="C:\opt\phantomjs-1.9.8-windows\bin\phantomjs.exe" name="PhantomJS" mode=ENABLE
