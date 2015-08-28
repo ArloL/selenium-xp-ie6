92995eb0a801020188b3cd54e597bf445ee8894d
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-28 11:45:47 +0200

    Formatting

--- provision-selenium.ps1
+++ provision-selenium.ps1
@@ -4,4 +4,5 @@ $java_executable = Get-Command java | Select-Object -ExpandProperty Definition
 netsh firewall add allowedprogram program=$java_executable name="Java(TM) Platform SE Binary" mode=ENABLE
 
 md C:\opt -Force
+
 Copy-Item "$($scriptDir)\selenium-server-standalone-2.47.1.jar" "C:\opt"
