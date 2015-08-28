c734d04bd7fbe1f54278260c63025d705a144d2e
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-28 17:00:22 +0200

    Bind correctly to IP

--- provision-phantomjs-node.ps1
+++ provision-phantomjs-node.ps1
@@ -2,7 +2,7 @@ param([String]$hubIp, [String]$bindIp)
 
 $bat = @'
 @echo off
-C:\opt\phantomjs-2.0.0-windows\bin\phantomjs.exe --webdriver=8080 --webdriver-selenium-grid-hub=http://{hubIp}:4444 --ip={bindIp}
+C:\opt\phantomjs-2.0.0-windows\bin\phantomjs.exe --webdriver={bindIp}:8080 --webdriver-selenium-grid-hub=http://{hubIp}:4444
 '@
 
 $bat = $bat.Replace("{hubIp}", $hubIp)
