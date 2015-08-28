f11283566df1ae87754f00bd7bb20a3d4bc3b168
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-28 11:46:19 +0200

    Make provision-phantomjs.ps1 only unzip phantomjs

--- provision-phantomjs.ps1
+++ provision-phantomjs.ps1
@@ -1,5 +1,3 @@
-param([String]$hubIp, [String]$bindIp)
-
 $scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
 
 . $scriptDir\Unzip.ps1
@@ -7,19 +5,3 @@ $scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
 md C:\opt -Force
 
 Unzip "$($scriptDir)\phantomjs-2.0.0-windows.zip" "C:\opt" $true
-
-$bat = @'
-@echo off
-C:\opt\phantomjs-2.0.0-windows\bin\phantomjs.exe --webdriver=8080 --webdriver-selenium-grid-hub=http://{hubIp}:4444 --ip={bindIp}
-'@
-
-$bat = $bat.Replace("{hubIp}", $hubIp)
-$bat = $bat.Replace("{bindIp}", $bindIp)
-
-$startupFolder = "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup"
-if (!(Test-Path $startupFolder)) {
-    $startupFolder = "$($env:ALLUSERSPROFILE)\Microsoft\Windows\Start Menu\Programs\Startup"
-}
-
-$bat | Out-File "$($startupFolder)\phantomjs.bat" -Encoding "Default" -Force
-
