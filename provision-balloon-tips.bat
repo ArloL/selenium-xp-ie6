9a03202e4555baffa0ab2f23e26d33f1ea82319d
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-28 10:42:02 +0200

    Use correct registry key

--- provision-balloon-tips.bat
+++ provision-balloon-tips.bat
@@ -1,2 +1,2 @@
 @echo off
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advaned" /v EnableBalloonTips /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v EnableBalloonTips /t REG_DWORD /d 0 /f
