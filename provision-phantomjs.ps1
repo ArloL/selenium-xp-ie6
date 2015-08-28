697c074c3c176e50012dde41d93e37754c7eac0e
Arlo O'Keeffe <mailtoarlo@gmail.com>
2015-08-28 11:20:02 +0200

    Add first version of provision-phantomjs.ps1

---
+++ provision-phantomjs.ps1
@@ -0,0 +1,19 @@
+param([String]$hubIp, [String]$bindIp)
+
+$scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+
+. $scriptDir\Unzip.ps1
+
+Unzip "$($scriptDir)\phantomjs-2.0.0-windows.zip" "C:\" $true
+
+$bat = @'
+@echo off
+C:\phantomjs-2.0.0-windows\bin\phantomjs.exe --webdriver=8080 --webdriver-selenium-grid-hub=http://{hubIp}:4444 --ip={bindIp}
+'@
+
+$bat = $bat.Replace("{hubIp}", $hubIp)
+$bat = $bat.Replace("{bindIp}", $bindIp)
+
+if (Test-Path "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup") {
+    $bat | Out-File "$($env:ALLUSERSPROFILE)\Start Menu\Programs\Startup\selenium-hub.bat" -Encoding "Default" -Force
+}
