49d243c04d9cdd96d72c7ed08e74606455a87cf5
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 13:46:29 +0100

    Do not use proxy for local addresses

--- provision-win10-proxy.bat
+++ provision-win10-proxy.bat
@@ -1,4 +1,5 @@
 @echo on
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 10.40.1.14:3128 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "<local>" /f
 exit /b 0
