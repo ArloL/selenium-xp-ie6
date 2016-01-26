31b66e44caa69ea0bcade8872e1a4d45eac8827a
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 15:40:31 +0100

    Add explanatory comment

--- provision-win10-proxy.ps1
+++ provision-win10-proxy.ps1
@@ -2,6 +2,7 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v Pr
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 10.40.1.14:3128 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "<local>" /f
 
+# we have to instantiate IE to commit the registry settings
 $ie = New-Object -ComObject internetexplorer.application
 $ie.Quit()
 
