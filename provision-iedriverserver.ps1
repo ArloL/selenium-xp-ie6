0c985f78f40d50df235e83583d4b3274f592666f
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 11:24:45 +0100

    Use latest IEDriver

--- provision-iedriverserver.ps1
+++ provision-iedriverserver.ps1
@@ -2,4 +2,4 @@ $scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
 
 . $scriptDir\Unzip.ps1
 
-Unzip "$($scriptDir)\IEDriverServer_Win32_2.46.0.zip" "C:\Windows\system32" $true
+Unzip "$($scriptDir)\IEDriverServer_Win32_2.49.0.zip" "C:\Windows\system32" $true
