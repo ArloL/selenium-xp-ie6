1fa17e4b8ba99aca3998683d63841d0cb8825b6a
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 11:10:09 +0100

    Update prerequisites

--- download-prerequisites.sh
+++ download-prerequisites.sh
@@ -3,25 +3,25 @@
 set -o errexit
 set -o xtrace
 
-wget --continue --timestamping http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
-shasum --check selenium-server-standalone-2.47.1.jar.sha1
+wget --continue --timestamping http://selenium-release.storage.googleapis.com/2.49/selenium-server-standalone-2.49.1.jar
+shasum --check selenium-server-standalone-2.49.1.jar.sha1
 
-wget --continue --timestamping http://selenium-release.storage.googleapis.com/2.47/IEDriverServer_Win32_2.47.0.zip
-shasum --check IEDriverServer_Win32_2.47.0.zip.sha1
+wget --continue --timestamping http://selenium-release.storage.googleapis.com/2.49/IEDriverServer_Win32_2.49.0.zip
+shasum --check IEDriverServer_Win32_2.49.0.zip.sha1
 
 wget --continue --timestamping http://selenium-release.storage.googleapis.com/2.46/IEDriverServer_Win32_2.46.0.zip
 shasum --check IEDriverServer_Win32_2.46.0.zip.sha1
 
-wget --continue --no-cookies --header="Cookie: oraclelicense=accept-securebackup-cookie" --output-document=jre-8u60-windows-i586.exe http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-windows-i586.exe
-shasum --check jre-8u60-windows-i586.exe.sha1
+wget --continue --no-cookies --header="Cookie: oraclelicense=accept-securebackup-cookie" --output-document=jre-8u72-windows-i586.exe http://download.oracle.com/otn-pub/java/jdk/8u72-b15/jre-8u72-windows-i586.exe
+shasum --check jre-8u72-windows-i586.exe.sha256
 
-wget --continue --no-cookies --header="Cookie: oraclelicense=accept-securebackup-cookie" --output-document=jre-8u60-windows-x64.exe http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-windows-x64.exe
-shasum --check jre-8u60-windows-x64.exe.sha1
+wget --continue --no-cookies --header="Cookie: oraclelicense=accept-securebackup-cookie" --output-document=jre-8u72-windows-x64.exe http://download.oracle.com/otn-pub/java/jdk/8u72-b15/jre-8u72-windows-x64.exe
+shasum --check jre-8u72-windows-x64.exe.sha256
 
-if [ ! -f phantomjs-2.0.0-windows.zip ]; then
-    wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.0.0-windows.zip
+if [ ! -f phantomjs-2.1.1-windows.zip ]; then
+    wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip
 fi
-shasum --check phantomjs-2.0.0-windows.zip.sha256
+shasum --check phantomjs-2.1.1-windows.zip.sha256
 
 if [ ! -f phantomjs-1.9.8-windows.zip ]; then
     wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-windows.zip
