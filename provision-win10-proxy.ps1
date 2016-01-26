d976ff654cf1fe1a9db9b1eb1fa7c048800d1ab1
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 16:13:45 +0100

    Set proxy per user

--- provision-win10-proxy.ps1
+++ provision-win10-proxy.ps1
@@ -1,3 +1,5 @@
+reg add "HKLM\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxySettingsPerUser /t REG_DWORD /d 1 /f
+
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 10.40.1.14:3128 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "<local>" /f
