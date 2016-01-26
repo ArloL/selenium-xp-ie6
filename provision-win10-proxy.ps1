434ca4c28388462f152f7d9aba654cbcacb7c757
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 16:32:03 +0100

    Set winhttp proxy explicitly instead of importing from IE

--- provision-win10-proxy.ps1
+++ provision-win10-proxy.ps1
@@ -4,8 +4,4 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v Pr
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 10.40.1.14:3128 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "<local>" /f
 
-# we have to instantiate IE to commit the registry settings
-$ie = New-Object -ComObject internetexplorer.application
-$ie.Quit()
-
-netsh winhttp import proxy source=ie
+netsh winhttp set proxy 10.40.1.14:3128 "<local>"
