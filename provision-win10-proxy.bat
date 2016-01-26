b80b4c6023303df39fdb6b9c588a7b0eaf368ab1
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 13:21:27 +0100

    Add provision-win10-proxy.bat

---
+++ provision-win10-proxy.bat
@@ -0,0 +1,4 @@
+@echo on
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d 10.40.1.14:3128 /f
+exit /b 0
