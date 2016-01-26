97cc43917dfece46fc2ddc38bba0ef5dbd0eba55
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 11:24:51 +0100

    Use latest jre

--- provision-java.ps1
+++ provision-java.ps1
@@ -5,6 +5,6 @@ if ([IntPtr]::size -eq 4) {
     $java_architecture = "i586"
 }
 
-if (!(Test-Path "C:\Program Files\Java\jre1.8.0_60")) {
-    Start-Process -FilePath "$($scriptDir)\jre-8u60-windows-$($java_architecture).exe" -ArgumentList "/s","INSTALL_SILENT=1","AUTO_UPDATE=0","WEB_JAVA=0","REBOOT=0","NOSTARTMENU=1","SPONSORS=0" -Wait -Passthru
+if (!(Test-Path "C:\Program Files\Java\jre1.8.0_72")) {
+    Start-Process -FilePath "$($scriptDir)\jre-8u72-windows-$($java_architecture).exe" -ArgumentList "/s","INSTALL_SILENT=1","AUTO_UPDATE=0","WEB_JAVA=0","REBOOT=0","NOSTARTMENU=1","SPONSORS=0" -Wait -Passthru
 }
