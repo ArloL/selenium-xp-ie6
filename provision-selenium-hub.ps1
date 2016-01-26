4dd1f3295586e66a6ae50e2ae5203e451b4061bc
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-26 11:25:07 +0100

    Use latest selenium server

--- provision-selenium-hub.ps1
+++ provision-selenium-hub.ps1
@@ -1,6 +1,6 @@
 $bat = @'
 @echo off
-java -jar C:\opt\selenium-server-standalone-2.47.1.jar -role hub
+java -jar C:\opt\selenium-server-standalone-2.49.1.jar -role hub
 '@
 
 $bat | Out-File "$([Environment]::GetFolderPath("Startup"))\selenium-hub.bat" -Encoding "Default" -Force
