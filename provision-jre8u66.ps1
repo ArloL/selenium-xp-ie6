853f52c4e913ebd6785e0ef2047daa0d2bead932
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-27 15:27:33 +0100

    Add more jre install scripts

---
+++ provision-jre8u66.ps1
@@ -0,0 +1,13 @@
+$scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+
+$java_architecture = "x64"
+if ([IntPtr]::size -eq 4) {
+    $java_architecture = "i586"
+}
+
+if (!(Test-Path "C:\Program Files\Java\jre1.8.0_66")) {
+    $process = Start-Process -FilePath "$($scriptDir)\jre-8u66-windows-$($java_architecture).exe" -ArgumentList "/s","INSTALL_SILENT=1","AUTO_UPDATE=0","WEB_ANALYTICS=0","REBOOT=0","SPONSORS=0" -Wait -Passthru
+    if ($process.ExitCode -ne 0) {
+        throw "Error installing jre: " + $process.ExitCode
+    }
+}
