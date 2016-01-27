e79a874731de1e45c0eefb39fdd9a29f33670a3f
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-01-27 14:20:53 +0100

    update jre provision scripts

--- provision-jre7.ps1
+++ provision-jre7.ps1
@@ -6,5 +6,5 @@ if ([IntPtr]::size -eq 4) {
 }
 
 if (!(Test-Path "C:\Program Files\Java\jre1.7.0_80")) {
-    Start-Process -FilePath "$($scriptDir)\jre-7u80-windows-$($java_architecture).exe" -ArgumentList "/s","INSTALL_SILENT=1","AUTO_UPDATE=0","WEB_JAVA=1","WEB_ANALYTICS=0","REBOOT=0","NOSTARTMENU=0","SPONSORS=0" -Wait -Passthru
+    Start-Process -FilePath "$($scriptDir)\jre-7u80-windows-$($java_architecture).exe" -ArgumentList "/s","SPONSORS=0" -Wait -Passthru
 }
--- provision-jre8.ps1
+++ provision-jre8.ps1
@@ -6,7 +6,7 @@ if ([IntPtr]::size -eq 4) {
 }
 
 if (!(Test-Path "C:\Program Files\Java\jre8")) {
-    $process = Start-Process -FilePath "$($scriptDir)\jre-8-windows-$($java_architecture).exe" -ArgumentList "/s","INSTALL_SILENT=1","AUTO_UPDATE=0","WEB_JAVA=1","WEB_ANALYTICS=0","REBOOT=0","SPONSORS=0" -Wait -Passthru
+    $process = Start-Process -FilePath "$($scriptDir)\jre-8-windows-$($java_architecture).exe" -ArgumentList "/s","INSTALL_SILENT=1","AUTO_UPDATE=0","WEB_ANALYTICS=0","REBOOT=0","SPONSORS=0" -Wait -Passthru
     if ($process.ExitCode -ne 0) {
         throw "Error installing jre: " + $process.ExitCode
     }
