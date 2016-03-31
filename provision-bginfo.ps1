45245f5dc518300d6a67c0f43cfdde1bfa139d5f
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-03-31 14:59:10 +0200

    Use [string]::Empty instead of ""

--- provision-bginfo.ps1
+++ provision-bginfo.ps1
@@ -1,4 +1,4 @@
 reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v bginfo /f
 reg add "HKCU\Control Panel\Colors" /v Background /t REG_SZ /d "0 99 177" /f
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d [string]::Empty /f
 %SystemRoot%\System32\RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
