25157d3551dcb63d8a65890cf03e50e8d8dac7a1
Arlo O'Keeffe <a.okeeffe@herbrand.de>
2016-03-31 13:34:23 +0200

    update proxy scripts

--- provision-win10-proxy-query.ps1
+++
@@ -1,3 +0,0 @@
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ
-reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ
